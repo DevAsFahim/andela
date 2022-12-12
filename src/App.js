e870e5267254a9c592cb8b221466bdb47d33c320
ASFahim007 <devasfahim@gmail.com>
2022-12-12 11:00:37 +0600

    first form

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import formLogo from './assets/image/formLogo.png'
 import formImg from './assets/image/formImg.png'
+import { FaEnvelope, FaMapMarkerAlt, FaUserAlt } from "react-icons/fa";
 
 function App() {
   return (
     <div>
       <div className="form_container">
-        <div className="form_left py-10">
+        <div className="form_left">
           <img className='form-logo' src={formLogo} alt="" />
           <img className='form-img' src={formImg} alt="" />
           <div className="form_left_content text-center">
@@ -16,6 +17,44 @@ function App() {
           </div>
         </div>
         <div className="form_right">
+
+          <div className="form_content">
+            <div className="step_text">
+              <h4>Becoming a <span>part of our global community</span> has never been easier</h4>
+            </div>
+            <form>
+              <div className='input_field'>
+                <label className="label">First name</label>
+                <div className="input">
+                  <FaUserAlt className='icon'></FaUserAlt>
+                  <input type="text" />
+                </div>
+              </div>
+              <div className='input_field'>
+                <label className="label">Last name</label>
+                <div className="input">
+                  <FaUserAlt className='icon'></FaUserAlt>
+                  <input type="text" />
+                </div>
+              </div>
+              <div className='input_field'>
+                <label className="label">Email</label>
+                <div className="input">
+                  <FaEnvelope className='icon'></FaEnvelope>
+                  <input type="email" />
+                </div>
+              </div>
+              <div className='input_field'>
+                <label className="label">Country</label>
+                <div className="input">
+                  <FaMapMarkerAlt className='icon'></FaMapMarkerAlt>
+                  <select>
+                    <option value="1">Bangladesh</option>
+                  </select>
+                </div>
+              </div>
+            </form>
+          </div>
         </div>
       </div>
     </div>
